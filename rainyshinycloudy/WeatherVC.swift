1937cfdcb18963e92d5b0648a8b8301b7d0aadca
Andrew Miller <milldawg@github.com>
2018-03-19 00:07:45 +0000

    Started basic UI template

--- rainyshinycloudy/WeatherVC.swift
+++ rainyshinycloudy/WeatherVC.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
